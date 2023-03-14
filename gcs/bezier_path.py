593bc16a305a5aa107ea5a5a41c3e573940c48a4
mpetersen94 <markpetersen@g.harvard.edu>
2023-03-14 15:42:08 -0400

    Clean up BezierPathGcs and test in 2d examples

--- gcs/bezier_path.py
+++ gcs/bezier_path.py
@@ -45,7 +45,7 @@ class BezierPathGCS(BaseGCS):
         self.continuity = continuity
         assert continuity < order
 
-        self.time_scaling_set = HPolyhedron.MakeBox([0], [1e3])
+        self.time_scaling_set = HPolyhedron.MakeBox([1e-3], [1e3])
 
         # Formulate edge costs and constraints
         self.r_control = MakeMatrixContinuousVariable(
@@ -87,8 +87,8 @@ class BezierPathGCS(BaseGCS):
             for deriv, c_con in enumerate(self.contin_constraints):
                 edge.AddConstraint(Binding[Constraint](
                         c_con,
-                        np.append(u.x()[-self.dimension*(deriv+1)-1:-1],
-                                  v.x()[:self.dimension*(deriv+1)])
+                        np.concatenate((u.x()[-self.dimension*(deriv+1)-1:-1],
+                                        v.x()[:self.dimension*(deriv+1)]))
                     ))
 
 
@@ -194,7 +194,7 @@ class BezierPathGCS(BaseGCS):
         for edge in target_edges:    
             for jj in range(self.dimension):
                 edge.AddConstraint(
-                    edge.xu()[-(self.dimension + self.order + 1) + jj] == edge.xv()[jj])
+                    edge.xu()[-(self.dimension + 1) + jj] == edge.xv()[jj])
 
             # if velocity is not None:
             #     edge.AddConstraint(Binding[Constraint](final_velocity_con, edge.xu()))
@@ -217,87 +217,20 @@ class BezierPathGCS(BaseGCS):
         # Extract trajectory control points
         knots = np.zeros(self.order + 1)
         path_control_points = []
-        time_control_points = []
         for edge in best_path:
             if edge.v() == self.target:
                 knots = np.concatenate((knots, [knots[-1]]))
                 path_control_points.append(best_result.GetSolution(edge.xv()))
-                time_control_points.append(np.array([best_result.GetSolution(edge.xu())[-1]]))
                 break
-            edge_time = knots[-1] + 1.
+            edge_time = knots[-1] + best_result.GetSolution(edge.xv())[-1]
             knots = np.concatenate((knots, np.full(self.order, edge_time)))
-            edge_path_points = np.reshape(best_result.GetSolution(edge.xv())[:-(self.order + 1)],
+            edge_path_points = np.reshape(best_result.GetSolution(edge.xv())[:-1],
                                              (self.dimension, self.order + 1), "F")
-            edge_time_points = best_result.GetSolution(edge.xv())[-(self.order + 1):]
             for ii in range(self.order):
                 path_control_points.append(edge_path_points[:, ii])
-                time_control_points.append(np.array([edge_time_points[ii]]))
-
-        offset = time_control_points[0].copy()
-        for ii in range(len(time_control_points)):
-            time_control_points[ii] -= offset
 
         path_control_points = np.array(path_control_points).T
-        time_control_points = np.array(time_control_points).T
 
         path = BsplineTrajectory(BsplineBasis(self.order + 1, knots), path_control_points)
-        time_traj = BsplineTrajectory(BsplineBasis(self.order + 1, knots), time_control_points)
 
-        return BezierTrajectory(path, time_traj), results_dict
-
-class BezierTrajectory:
-    def __init__(self, path_traj, time_traj):
-        assert path_traj.start_time() == time_traj.start_time()
-        assert path_traj.end_time() == time_traj.end_time()
-        self.path_traj = path_traj
-        self.time_traj = time_traj
-        self.start_s = path_traj.start_time()
-        self.end_s = path_traj.end_time()
-
-    def invert_time_traj(self, t):
-        if t <= self.start_time():
-            return self.start_s
-        if t >= self.end_time():
-            return self.end_s
-        error = lambda s: self.time_traj.value(s)[0, 0] - t
-        res = root_scalar(error, bracket=[self.start_s, self.end_s])
-        return np.min([np.max([res.root, self.start_s]), self.end_s])
-
-    def value(self, t):
-        return self.path_traj.value(self.invert_time_traj(np.squeeze(t)))
-
-    def vector_values(self, times):
-        s = [self.invert_time_traj(t) for t in np.squeeze(times)]
-        return self.path_traj.vector_values(s)
-
-    def EvalDerivative(self, t, derivative_order=1):
-        if derivative_order == 0:
-            return self.value(t)
-        elif derivative_order == 1:
-            s = self.invert_time_traj(np.squeeze(t))
-            s_dot = 1./self.time_traj.EvalDerivative(s, 1)[0, 0]
-            r_dot = self.path_traj.EvalDerivative(s, 1)
-            return r_dot * s_dot
-        elif derivative_order == 2:
-            s = self.invert_time_traj(np.squeeze(t))
-            s_dot = 1./self.time_traj.EvalDerivative(s, 1)[0, 0]
-            h_ddot = self.time_traj.EvalDerivative(s, 2)[0, 0]
-            s_ddot = -h_ddot*(s_dot**3)
-            r_dot = self.path_traj.EvalDerivative(s, 1)
-            r_ddot = self.path_traj.EvalDerivative(s, 2)
-            return r_ddot * s_dot * s_dot + r_dot * s_ddot
-        else:
-            raise ValueError()
-
-
-    def start_time(self):
-        return self.time_traj.value(self.start_s)[0, 0]
-
-    def end_time(self):
-        return self.time_traj.value(self.end_s)[0, 0]
-
-    def rows(self):
-        return self.path_traj.rows()
-
-    def cols(self):
-        return self.path_traj.cols()
+        return path, results_dict
